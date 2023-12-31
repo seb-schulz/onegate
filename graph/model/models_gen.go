c54dfb96bf42e8bf2e114e2d79517c16268b3612
Sebastian Schulz <16666034+seb-schulz@users.noreply.github.com>
2023-12-31 01:09:15 +0100

    Regenerate schema of graphQL

--- graph/model/models_gen.go
+++ graph/model/models_gen.go
@@ -2,9 +2,15 @@
 
 package model
 
+type Mutation struct {
+}
+
 type PubKeyCredParam struct {
 	Type string `json:"type"`
 	Alg  int    `json:"alg"`
+}
+
+type Query struct {
 }
 
 type RelyingParty struct {
