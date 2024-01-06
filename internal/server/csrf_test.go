f65b207e26a3cae5e75ab788fed20f224af6a792
Sebastian Schulz <16666034+seb-schulz@users.noreply.github.com>
2024-01-06 19:26:14 +0100

    Add tests for csrf middleware

---
+++ internal/server/csrf_test.go
@@ -0,0 +1,50 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func newRequestWithHeader(method string, header *http.Header) *http.Request {
+	req := httptest.NewRequest(method, "http://example.com/foo", nil)
+
+	if header != nil {
+		req.Header = *header
+	}
+
+	return req
+}
+
+func TestCsrfMitigationMiddleware(t *testing.T) {
+	handler := csrfMitigationMiddleware(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintln(w, "Hello, client")
+		}))
+
+	for _, tc := range []struct {
+		expect int
+		req    *http.Request
+	}{
+		{http.StatusUnauthorized, newRequestWithHeader("GET", nil)},
+		{http.StatusUnauthorized, newRequestWithHeader("POST", nil)},
+		{http.StatusUnauthorized, newRequestWithHeader("GET", &http.Header{"X-Random-Header": []string{"foobar"}})},
+		{http.StatusUnauthorized, newRequestWithHeader("POST", &http.Header{"X-Random-Header": []string{"foobar"}})},
+		{http.StatusUnauthorized, newRequestWithHeader("GET", &http.Header{"X-Onegate-Csrf-Protection": []string{"abc", "0"}})},
+		{http.StatusUnauthorized, newRequestWithHeader("POST", &http.Header{"X-Onegate-Csrf-Protection": []string{"abc", "0"}})},
+		{http.StatusUnauthorized, newRequestWithHeader("GET", &http.Header{"X-Onegate-Csrf-Protection": []string{"abc"}})},
+		{http.StatusUnauthorized, newRequestWithHeader("GET", &http.Header{"X-Onegate-Csrf-Protection": []string{"0"}})},
+		{http.StatusOK, newRequestWithHeader("POST", &http.Header{"X-Onegate-Csrf-Protection": []string{"1"}})},
+	} {
+		w := httptest.NewRecorder()
+		handler.ServeHTTP(w, tc.req)
+
+		resp := w.Result()
+
+		if tc.expect != resp.StatusCode {
+			t.Errorf("expected status code %v but got %v", tc.expect, resp.StatusCode)
+		}
+	}
+
+}
