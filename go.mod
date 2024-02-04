203b0b148e1969a3ffd00b76395126d50608fdb1
Sebastian Schulz <16666034+seb-schulz@users.noreply.github.com>
2024-02-04 21:28:43 +0100

    Add oAuth2 as mock

--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/fxamacker/cbor/v2 v2.5.0 // indirect
 	github.com/go-webauthn/x v0.1.6 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-tpm v0.9.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
@@ -28,6 +29,8 @@ require (
 	golang.org/x/crypto v0.18.0 // indirect
 	golang.org/x/exp v0.0.0-20240119083558-1b970713d09a // indirect
 	golang.org/x/term v0.16.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	rsc.io/qr v0.2.0 // indirect
 )
 
@@ -36,6 +39,7 @@ require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.3 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-chi/chi/v5 v5.0.11
+	github.com/go-oauth2/oauth2/v4 v4.5.2
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/go-webauthn/webauthn v0.10.0
 	github.com/golang-jwt/jwt/v5 v5.2.0
@@ -60,6 +64,7 @@ require (
 	github.com/xrash/smetrics v0.0.0-20231213231151-1d8dd44e695e // indirect
 	golang.org/x/mod v0.14.0 // indirect
 	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/oauth2 v0.16.0
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/tools v0.17.0 // indirect
---
+++ internal/auth/auth_test.go
@@ -0,0 +1,354 @@
+package auth
+
+import (
+	"context"
+	"crypto/subtle"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strconv"
+	"testing"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-oauth2/oauth2/v4/errors"
+	"github.com/seb-schulz/onegate/internal/sessionmgr"
+
+	"golang.org/x/oauth2"
+)
+
+type (
+	client struct {
+		ID          uint
+		Secret      string
+		RedirectURI string
+	}
+
+	clientFinder interface {
+		firstClient(ctx context.Context, id uint) (*client, error)
+	}
+
+	authorization struct {
+		ID            uint
+		clientID      uint
+		client        client
+		userID        uint
+		state         string
+		code          string
+		codeChallenge string
+	}
+
+	authorizationRequestHandler struct {
+		clientFinder     clientFinder
+		authorizationMgr interface {
+			createAuthorization(ctx context.Context, client *client, state string, codeChallenge string) error
+		}
+		loginUrl url.URL
+	}
+
+	authorizationResponseHandler struct {
+		authorizationMgr interface {
+			updateAuthorizationUserID(ctx context.Context, userID uint) error
+			authorizationFromContext(ctx context.Context) *authorization
+		}
+	}
+
+	tokenHandler struct {
+		clientFinder     clientFinder
+		authorizationMgr interface {
+			getAuthorizationByCode(ctx context.Context, code string) (*authorization, error)
+		}
+	}
+)
+
+func (a *authorization) IDStr() string {
+	return fmt.Sprint(a.ID)
+}
+
+func (c *client) verifySecret(s string) error {
+	if c.Secret != s {
+		return fmt.Errorf("secret is not equal")
+	}
+	return nil
+}
+
+func (auth authorizationRequestHandler) checkResponseType(response_type string) error {
+	if response_type != "code" {
+		return errors.ErrUnsupportedResponseType
+	}
+	return nil
+}
+
+func (auth authorizationRequestHandler) parseClientID(r *http.Request) (uint, error) {
+	cIDStr := r.FormValue("client_id")
+	if !(r.Method == "GET" || r.Method == "POST") ||
+		cIDStr == "" {
+		return 0, errors.ErrInvalidRequest
+	}
+
+	id, err := strconv.ParseUint(cIDStr, 10, 32)
+	if err != nil {
+		return 0, errors.ErrInvalidRequest
+	}
+
+	return uint(id), nil
+}
+
+func (auth authorizationRequestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// log.Printf("Query params: %#v", r.URL.Query())
+	if err := auth.checkResponseType(r.FormValue("response_type")); err != nil {
+		http.Error(w, errors.Descriptions[err], errors.StatusCodes[err])
+		return
+	}
+
+	if r.FormValue("code_challenge_method") != "S256" {
+		panic("not implemented yet")
+	}
+
+	clientID, err := auth.parseClientID(r)
+	if err != nil {
+		http.Error(w, errors.Descriptions[err], errors.StatusCodes[err])
+		return
+	}
+
+	client, err := auth.clientFinder.firstClient(r.Context(), clientID)
+	if err != nil {
+		http.Error(w, errors.Descriptions[errors.ErrInvalidRequest], errors.StatusCodes[err])
+	}
+
+	if auth.authorizationMgr.createAuthorization(r.Context(), client, r.FormValue("state"), r.FormValue("code_challenge")); err != nil {
+		panic("not implemented yet")
+	}
+
+	http.Redirect(w, r, fmt.Sprint(&auth.loginUrl), http.StatusSeeOther)
+}
+
+func (auth *authorizationResponseHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	auth.authorizationMgr.updateAuthorizationUserID(r.Context(), 1)
+	authReq := auth.authorizationMgr.authorizationFromContext(r.Context())
+	q := url.Values{}
+	q.Add("code", authReq.code)
+	q.Add("state", authReq.state)
+	http.Redirect(w, r, fmt.Sprintf("%v?%v", authReq.client.RedirectURI, q.Encode()), http.StatusFound)
+}
+
+func (th *tokenHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// s, _ := io.ReadAll(r.Body)
+	// log.Println(string(s))
+	client, err := th.verifyClient(r)
+	if err != nil {
+		log.Printf("cannot verify client: %v", err)
+		http.Error(w, "invalid", http.StatusBadRequest)
+		return
+	}
+
+	if err := th.checkGrantType(r); err != nil {
+		http.Error(w, errors.Descriptions[err], errors.StatusCodes[err])
+		return
+	}
+
+	authReq, err := th.authorizationMgr.getAuthorizationByCode(r.Context(), r.FormValue("code"))
+	if err != nil {
+		log.Printf("authorization not found: %v", err)
+		http.Error(w, "not implemented yet", http.StatusNotImplemented)
+		return
+	}
+
+	if err := th.checkCodeChallenge(r, authReq); err != nil {
+		log.Println("missmach between authorization and client")
+		http.Error(w, "not implemented yet", http.StatusNotImplemented)
+		return
+	}
+
+	if authReq.clientID != client.ID {
+		log.Println("missmach between authorization and client")
+		http.Error(w, "not implemented yet", http.StatusNotImplemented)
+		return
+	}
+
+	b, err := json.Marshal(struct {
+		AccessToken string `json:"access_token,omitempty"`
+		TokenType   string `json:"token_type,omitempty"`
+		ExpiresIn   int    `json:"expires_in,omitempty"`
+		IDToken     string `json:"id_token,omitempty"`
+	}{"xyz123", "Bearer", 10 * 60, "abc"})
+	if err != nil {
+		log.Printf("cannot generate token: %v", err)
+		http.Error(w, "not implemented yet", http.StatusNotImplemented)
+		return
+	}
+
+	log.Printf("Form params: %#v", r.Form)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Cache-Control", "no-store")
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}
+
+func (th *tokenHandler) checkCodeChallenge(r *http.Request, auth *authorization) error {
+	cv := r.FormValue("code_verifier")
+	if subtle.ConstantTimeCompare([]byte(auth.codeChallenge), []byte(oauth2.S256ChallengeFromVerifier(cv))) != 1 {
+		return fmt.Errorf("missmatch of code challenge")
+	}
+	return nil
+}
+
+func (th *tokenHandler) checkGrantType(r *http.Request) error {
+	if r.FormValue("grant_type") != "authorization_code" {
+		return errors.ErrInvalidGrant
+	}
+	return nil
+}
+
+func (th *tokenHandler) verifyClient(r *http.Request) (*client, error) {
+	var clientID, secret string
+
+	clientID, secret, ok := r.BasicAuth()
+	if !ok {
+		clientID = r.FormValue("client_id")
+		secret = r.FormValue("client_secret")
+	}
+
+	cID, err := strconv.ParseUint(clientID, 10, 32)
+	if err != nil {
+		return nil, fmt.Errorf("invalid client ID: %v", err)
+	}
+
+	client, err := th.clientFinder.firstClient(r.Context(), uint(cID))
+	if err != nil {
+		return nil, fmt.Errorf("cannot fetch client: %v", err)
+	}
+
+	if err := client.verifySecret(secret); err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
+type mockClientMgr []*client
+
+func (cm *mockClientMgr) firstClient(ctx context.Context, id uint) (*client, error) {
+	for _, c := range *cm {
+		if c.ID == id {
+			return c, nil
+		}
+	}
+	return nil, fmt.Errorf("not found")
+
+}
+
+type mockAuthorizationMgr struct {
+	*sessionmgr.StorageManager[*authorization]
+	currentAuthorization *authorization
+}
+
+func (auth *mockAuthorizationMgr) createAuthorization(ctx context.Context, client *client, state, codeChallenge string) error {
+	auth.currentAuthorization = &authorization{ID: 1, clientID: client.ID, client: *client, state: state, code: "1", codeChallenge: codeChallenge}
+	return nil
+}
+
+func (auth *mockAuthorizationMgr) updateAuthorizationUserID(ctx context.Context, userID uint) error {
+	auth.currentAuthorization.userID = 1
+	return nil
+}
+
+func (auth *mockAuthorizationMgr) getAuthorizationByCode(ctx context.Context, code string) (*authorization, error) {
+	return auth.currentAuthorization, nil
+}
+
+func (auth *mockAuthorizationMgr) authorizationFromContext(ctx context.Context) *authorization {
+	return auth.FromContext(ctx)
+}
+
+func TestAuthCodeFlow(t *testing.T) {
+	var (
+		code         string
+		expectedBody = "Done \\o/"
+	)
+
+	client_ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		code = r.FormValue("code")
+		if code == "" {
+			t.Errorf("expected non-empty authorization code")
+		}
+
+		state := r.FormValue("state")
+		if state != "state" {
+			t.Errorf("expected 'state' as state value but got: %#v", state)
+		}
+		fmt.Fprint(w, expectedBody)
+	}))
+	defer client_ts.Close()
+
+	mock := &mockAuthorizationMgr{}
+	mock.StorageManager = sessionmgr.NewStorage[*authorization]("authorization", func(ctx context.Context) (*authorization, error) {
+		return mock.currentAuthorization, nil
+	})
+
+	mockClientMgr := &mockClientMgr{&client{123, "secret", client_ts.URL}}
+
+	route := chi.NewRouter()
+	authorizationRequestHandler := &authorizationRequestHandler{
+		clientFinder:     mockClientMgr,
+		loginUrl:         url.URL{Path: "/callback"},
+		authorizationMgr: mock,
+	}
+	route.Get("/auth", authorizationRequestHandler.ServeHTTP)
+
+	authorizationResponseHandler := &authorizationResponseHandler{
+		authorizationMgr: mock,
+	}
+	route.With(mock.Handler).Get("/callback", authorizationResponseHandler.ServeHTTP)
+
+	tokenHandler := &tokenHandler{clientFinder: mockClientMgr, authorizationMgr: mock}
+	route.Post("/token", tokenHandler.ServeHTTP)
+
+	ts := httptest.NewServer(route)
+	defer ts.Close()
+
+	conf := &oauth2.Config{
+		ClientID:     "123",
+		ClientSecret: "secret",
+		Scopes:       []string{"openid"},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:   fmt.Sprintf("%v/auth", ts.URL),
+			TokenURL:  fmt.Sprintf("%v/token", ts.URL),
+			AuthStyle: oauth2.AuthStyleInParams,
+		},
+	}
+
+	// use PKCE to protect against CSRF attacks
+	// https://www.ietf.org/archive/id/draft-ietf-oauth-security-topics-22.html#name-countermeasures-6
+	verifier := oauth2.GenerateVerifier()
+	authUrl := conf.AuthCodeURL("state", oauth2.S256ChallengeOption(verifier))
+
+	res, err := http.Get(authUrl)
+	if err != nil {
+		t.Errorf("cannot get authorization token: %v", err)
+	}
+	greeting, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(greeting) != expectedBody {
+		t.Errorf("Got %#v instead of %#v", string(greeting), expectedBody)
+	}
+
+	ctx := context.Background()
+	tok, err := conf.Exchange(ctx, code, oauth2.VerifierOption(verifier))
+	if err != nil {
+		t.Fatal(err)
+	}
+	log.Println(tok)
+	log.Println(tok.Extra("id_token"))
+
+	// client := conf.Client(ctx, tok)
+	// client.Get("...")
+	// t.FailNow()
+}
