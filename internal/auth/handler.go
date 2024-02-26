c8029884e877679045e425bb45852427b2453800
Sebastian Schulz <16666034+seb-schulz@users.noreply.github.com>
2024-02-26 20:52:31 +0100

    Provide oAuth middleware while being in login modus

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -2,6 +2,8 @@ package auth
 
 import (
 	"context"
+	"fmt"
+	"log/slog"
 	"net/http"
 	"net/url"
 
@@ -32,4 +34,31 @@ func NewHandler() http.Handler {
 	route.Post("/token", tokenHandler.ServeHTTP)
 
 	return route
+}
+
+func RedirectWhenLoggedInAndAssigned(callbackURL string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			authReq, err := firstAuthorization(r.Context())
+			if authReq == nil {
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			if err != nil {
+				// TODO: Evaluate proper error page
+				slog.Warn(fmt.Sprintf("oAuth2 callback failed with: %v", err))
+				http.NotFound(w, r)
+				return
+			}
+
+			if authReq.InternalUserID == nil {
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			http.Redirect(w, r, callbackURL, http.StatusSeeOther)
+		})
+	}
 }
